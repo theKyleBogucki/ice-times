559a050fae05e96ec5dde7e8ab58b8af8c961926
theKyleBogucki <boguckik@VOXC02NX7L6G3QD.local>
2015-08-12 20:49:38 -0400

    responsive layout updates
    i have added a new css file for the mobile layout

--- js/index.js
+++ js/index.js
@@ -1,6 +1,31 @@
 jQuery(document).ready(function() {
 
+  var state = false;
 
+  function hideDropDown() {
+    state = false;
+    $("#nav").fadeOut();
+    $(".hamburger").text("=")
+  }
+
+  function showDropDown() {
+    state = true;
+    $("#nav").fadeIn();
+    $(".hamburger").text("X")
+  }
+
+
+function animateNav () {
+  if (state) {
+    hideDropDown();
+  } else {
+    showDropDown();
+  }
+}
+
+
+
+$(".hamburger").click(animateNav)
 
 
 
